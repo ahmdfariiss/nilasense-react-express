12de977af14e9650060d1a09a9a5a1860ad866a6
ahmdfariiss <afarisalaziz201@gmail.com>
2025-10-18 19:10:20 +0700

    Ganti port

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 
 // Inisialisasi aplikasi express
 const app = express();
-const PORT = process.env.PORT || 5000; // Gunakan port dari .env atau default ke 5000
+const PORT = process.env.PORT || 5001; // Gunakan port dari .env atau default ke 5000
 const authRoutes = require("./routes/authRoutes");
 const userRoutes = require("./routes/userRoutes");
 
